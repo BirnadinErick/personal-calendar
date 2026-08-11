c9029dc32a3705536f2c041b275853a86a6d8caa
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2026-08-11 12:24:36 +0200

    feat(store): add date and month state for calendar navigation

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -6,10 +6,14 @@ interface AppState {
   caldavUrl: string;
   caldavUser: string;
   isSyncing: boolean;
+  selectedDate: Date;
+  currentMonth: Date;
   setIsOnline: (status: boolean) => void;
   setActiveTab: (tab: 'calendar' | 'todos') => void;
   setCaldavCredentials: (url: string, user: string) => void;
   setIsSyncing: (syncing: boolean) => void;
+  setSelectedDate: (date: Date) => void;
+  setCurrentMonth: (month: Date) => void;
 }
 
 export const useStore = create<AppState>((set) => ({
@@ -18,8 +22,12 @@ export const useStore = create<AppState>((set) => ({
   caldavUrl: '',
   caldavUser: '',
   isSyncing: false,
+  selectedDate: new Date(),
+  currentMonth: new Date(),
   setIsOnline: (status) => set({ isOnline: status }),
   setActiveTab: (tab) => set({ activeTab: tab }),
   setCaldavCredentials: (url, user) => set({ caldavUrl: url, caldavUser: user }),
   setIsSyncing: (syncing) => set({ isSyncing: syncing }),
+  setSelectedDate: (date) => set({ selectedDate: date }),
+  setCurrentMonth: (month) => set({ currentMonth: month }),
 }));
